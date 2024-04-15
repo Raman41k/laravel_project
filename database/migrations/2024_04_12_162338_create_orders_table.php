d50b88ef267492f6f6e7d5ce42ed7b5f45479d23
raman41k <romangugel@gmail.com>
2024-04-15 16:21:48 +0300

    Add models, migrations, seeders, and factories for new entities

---
+++ database/migrations/2024_04_12_162338_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->unsignedBigInteger('worker_id');
+            $table->foreign('worker_id')->references('id')->on('workers')->onDelete('cascade');
+            $table->unsignedBigInteger('service_id');
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('cascade');
+            $table->unsignedBigInteger('timeslot');
+            $table->foreign('timeslot_id')->references('id')->on('schedules')->onDelete('cascade');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
